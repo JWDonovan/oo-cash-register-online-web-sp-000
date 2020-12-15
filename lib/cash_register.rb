f59a8950d48507b156b16e4435fc4280537acf67
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-15 21:02:52 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,13 +1,3 @@
 class CashRegister
-  attr_accessor :total
-
-  def initialize(discount = 0)
-    @total = 0
-    @discount = discount
-  end
-
-  def apply_discount
-    val = @discount / 100
-    @total -= val
-  end
+  
 end
